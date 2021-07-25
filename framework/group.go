ffde9df8f0e8992e8197168c3547e5d913326d31
yejianfeng <jianfengye110@gmail.com>
2021-07-25 11:21:21 +0800

    add group comment

--- framework/group.go
+++ framework/group.go
@@ -2,11 +2,13 @@ package framework
 
 // IGroup 代表前缀分组
 type IGroup interface {
+	// 实现HttpMethod方法
 	Get(string, ControllerHandler)
 	Post(string, ControllerHandler)
 	Put(string, ControllerHandler)
 	Delete(string, ControllerHandler)
 
+	// 实现嵌套group
 	Group(string) IGroup
 }
 
@@ -32,21 +34,25 @@ func (g *Group) Get(uri string, handler ControllerHandler) {
 	g.core.Get(uri, handler)
 }
 
+// 实现Post方法
 func (g *Group) Post(uri string, handler ControllerHandler) {
 	uri = g.getAbsolutePrefix() + uri
 	g.core.Post(uri, handler)
 }
 
+// 实现Put方法
 func (g *Group) Put(uri string, handler ControllerHandler) {
 	uri = g.getAbsolutePrefix() + uri
 	g.core.Put(uri, handler)
 }
 
+// 实现Delete方法
 func (g *Group) Delete(uri string, handler ControllerHandler) {
 	uri = g.getAbsolutePrefix() + uri
 	g.core.Delete(uri, handler)
 }
 
+// 获取当前group的绝对路径
 func (g *Group) getAbsolutePrefix() string {
 	if g.parent == nil {
 		return g.prefix
@@ -54,6 +60,7 @@ func (g *Group) getAbsolutePrefix() string {
 	return g.parent.getAbsolutePrefix() + g.prefix
 }
 
+// 实现 Group 方法
 func (g *Group) Group(uri string) IGroup {
 	cgroup := NewGroup(g.core, uri)
 	cgroup.parent = g
