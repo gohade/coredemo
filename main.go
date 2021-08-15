8b376cf9fa7fe328f805e102227383dd2603f4e4
yejianfeng <jianfengye110@gmail.com>
2021-08-15 22:20:12 +0800

    add cost

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"coredemo/framework/gin"
+	"coredemo/framework/middleware"
 	"log"
 	"net/http"
 	"os"
@@ -15,6 +16,7 @@ func main() {
 	core := gin.New()
 
 	core.Use(gin.Recovery())
+	core.Use(middleware.Cost())
 
 	registerRouter(core)
 	server := &http.Server{
