e25c18399b04957b6ddc313cf6a1b1eb9b9d6045
yejianfeng <jianfengye110@gmail.com>
2021-10-31 22:40:58 +0800

    update branch 21

--- framework/command/kernel.go
+++ framework/command/kernel.go
@@ -26,8 +26,6 @@ func AddKernelCommands(root *cobra.Command) {
 	root.AddCommand(initProviderCommand())
 	// middleware
 	root.AddCommand(initMiddlewareCommand())
-	// new
-	root.AddCommand(initNewCommand())
 	//
 	//// swagger
 	//swagger.IndexCommand.AddCommand(swagger.InitServeCommand())
